4d6def76c83b6a3ccf704ef25e4dbd5b356925c2
parthjani11 <parthjani620@gmail.com>
2026-07-20 10:59:49 +0530

    Update defines.svh

--- codes/defines.svh
+++ codes/defines.svh
@@ -2,5 +2,5 @@
 
 `define data_width     32
 `define data_depth     256
-`define addr_width     8          // $clog2(32) = 5 — hardcoded to avoid macro-expansion issues
+`define addr_width     8        
 `define num_transactions 150
